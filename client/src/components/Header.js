f8ed673e8e17b9636a865eefbb3fab14856f95e2
Oscar Trosseau <o.trosseau@laposte.net>
2023-02-24 10:16:19 +0100

    Add application name, header and home components

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -11,6 +11,18 @@ function Header() {
         <>
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div className="container-fluid">
+                    <a className="navbar-brand" href={url}>Marvel Characters</a>
+                    <span></span>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link" href="/browse">Browse</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/superteam">Superteam</a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </nav>
             <Outlet />
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,9 +4,11 @@
 const Home = () => {
   return <div className="container text-center py-5">
     <div className="display-1 text-light">
-      </div>
+      Marvel Characters
+    </div>
     <div className="display-5 text-light">
-      </div>
+      Start browsing the characters by clicking on "Browse" in the toolbar.
+    </div>
   </div>;
 };
 
