67263445fc1ebe886765997da05a692cca845d38
Oscar Trosseau <o.trosseau@laposte.net>
2023-02-24 10:15:15 +0100

    Fix loading more characters not finishing

--- client/src/api/searchCharacter.js
+++ client/src/api/searchCharacter.js
@@ -3,18 +3,15 @@
  * @param {number} page - The page of characters to fetch
  * @returns {Promise.<object>} The promise of an object containing the relevent informations concerning the fetch or an error message. 
  */
-export function fetchCharacters(page) {
-    if (!page || page === "") {
-        return { albums: [], fetchFinished: true };
-    }
-    return fetch("http://localhost:8000/characters?page=" + encodeURIComponent(page))
+export function fetchCharacters(page, abortController) {
+    return fetch("http://localhost:8000/characters?page=" + encodeURIComponent(page), {signal: abortController.signal})
         .then((res) => res.json())
         .then((data) => {
             if (data.error) {
                 console.error(data);
-                return { characters: [], fetchFinished: true };
+                return { characters: [], fetchStarted: false };
             }
-            return { characters: data, fetchFinished: true };
+            return { characters: data, fetchStarted: false };
         }).catch((error) => {
             return { error: error };
         });
