7907374183765284902e965de4c2de47aad34ffb
Oscar Trosseau <o.trosseau@laposte.net>
2023-02-24 15:13:11 +0100

    Add test parse Marvel API response

---
+++ server/src/tests/getCharactersQuery.test.js
@@ -0,0 +1,22 @@
+const { readFileSync } = require('fs');
+const { parseSearchResult } = require("./../marvel_api/getCharactersQuery");
+
+const JSON_MOCK_DATA = "./src/tests/mocks/rawBrowseResultMock.json";
+const JSON_MOCK_PARSED = "./src/tests/mocks/browseResultMockParsed.json";
+
+describe("Parse the data received from the Marvel api to get a list of characters", () => {
+    const mockData = JSON.parse(readFileSync(JSON_MOCK_DATA));
+    const mockParsed = JSON.parse(readFileSync(JSON_MOCK_PARSED));
+
+    test('A normal response', () => {
+        expect(parseSearchResult(mockData)).toEqual(mockParsed);
+    });
+
+    test('An empty response', () => {
+        expect(parseSearchResult({})).toStrictEqual([]);
+    });
+
+    test('An invalid response', () => {
+        expect(parseSearchResult({ "error": "invalid response" })).toStrictEqual([]);
+    });
+});
